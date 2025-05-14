6df2c13ac615e168722ca941c0188e84af39b6db
Pushpender <pushpenderrajputsp@gmail.com>
2025-05-14 11:23:14 +0530

    Add Netligy-plugins

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
+    
   },
   eslint: {
     ignoreDuringBuilds: true,
+    
   },
+  devIndicators: false,
   images: {
     remotePatterns: [
       {
